1b03f7fd1bff43cf9ef4b7d491c3a0b0e04b2d06
philipsdoctor <diomedestydeus@gmail.com>
2016-11-28 17:34:26 -0600

    Add some tests to demo how to test

--- project.clj
+++ project.clj
@@ -5,7 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [metosin/compojure-api "1.1.9" :exclusions [org.clojure/clojure]]
-                 [ring/ring-jetty-adapter "1.5.0"]]
+                 [ring/ring-jetty-adapter "1.5.0"]
+
+                 ;; this should be moved to a test profile, but this is just a demo
+                 [ring/ring-mock "0.3.0" :exclusions [ring/ring-codec]]]
   :main ^:skip-aot compojure-api-demo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- test/compojure_api_demo/core_test.clj
+++ test/compojure_api_demo/core_test.clj
@@ -1,7 +1,33 @@
 (ns compojure-api-demo.core-test
   (:require [clojure.test :refer :all]
-            [compojure-api-demo.core :refer :all]))
+            [compojure-api-demo.core :refer :all]
+            [ring.mock.request :as mock]
+            [cheshire.core :as json]
+            [compojure-api-demo.routes :as routes])
+  (:import (java.io InputStreamReader File)
+           (schema.utils ValidationError)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn api-request
+  ([method uri]
+   (-> (mock/request method uri)
+       (mock/content-type "application/json")))
+  ([method uri body]
+    (-> (api-request method uri)
+        (mock/body (json/generate-string body)))))
+
+(defn parse-json-body [response]
+  (json/parse-string (slurp (InputStreamReader. (:body response))) true))
+
+(deftest test-post-bar
+  (testing "parameters correctly parsed, validated, and passed"
+    (let [app (build-app)
+          req (api-request :post "/v1/bar?req-id=tako" {:totalTxBytes 10
+                                                        :planId "4123EF"
+                                                        :rxPercentage 98.02})
+          resp (app req)]
+      (is (= 200 (:status resp)))))
+  (testing "Not including required keys in the map creates a failure"
+    (let [app (build-app)
+          req (api-request :post "/v1/bar?req-id=tako" {:rxPercentage 98.02})
+          resp (app req)]
+      (is (= 400 (:status resp))))))
