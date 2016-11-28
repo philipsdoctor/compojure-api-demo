4007fd7f2f1551c2f10460fd01d025f9f61bdcc0
philipsdoctor <diomedestydeus@gmail.com>
2016-11-28 12:02:27 -0600

    Add a more interesting route to better show off Swagger and Schema

--- src/compojure_api_demo/routes.clj
+++ src/compojure_api_demo/routes.clj
@@ -3,6 +3,11 @@
             [schema.core :as schema-core]
             [ring.util.http-response :as http-response :refer [ok internal-server-error bad-request not-found content-type unsupported-media-type content-type]]))
 
+(schema-core/defschema BarBody
+  {:totalTxBytes schema-core/Num
+   :planId schema-core/Str
+   :rxPercentage schema-core/Num})
+
 (def app-routes
   (compojure-api/routes
    (compojure-api/context "/v1" []
@@ -20,6 +25,7 @@
                                 :summary "Mostly it just does Bar."
                                 :tags ["Another Tag"]
                                 :query-params [req-id :- (describe schema-core/Str "The request-id for tracking.")]
+                                :body [barBody (describe BarBody "A map of the state of the action, includes planId to match the action identifier, total bytes and current progress.")]
                                 :responses {:default {:schema {}}
                                             200 {:schema {}}}
                                 (prn "done")
